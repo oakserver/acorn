980072aa6722df1fd71d8de25979c1da5db7b810
Kitson Kelly <me@kitsonkelly.com>
2022-05-09 15:43:57 +1000

    chore: update to oak_commons 0.3.1

--- deps.ts
+++ deps.ts
@@ -1,19 +1,19 @@
 // Copyright 2022 the oak authors. All rights reserved.
 
-export { Cookies } from "https://deno.land/x/oak_commons@0.3.0/cookies.ts";
+export { Cookies } from "https://deno.land/x/oak_commons@0.3.1/cookies.ts";
 export {
   createHttpError,
   type HttpError,
   isHttpError,
-} from "https://deno.land/x/oak_commons@0.3.0/http_errors.ts";
-export { accepts } from "https://deno.land/x/oak_commons@0.3.0/negotiation.ts";
+} from "https://deno.land/x/oak_commons@0.3.1/http_errors.ts";
+export { accepts } from "https://deno.land/x/oak_commons@0.3.1/negotiation.ts";
 export {
   Status,
   STATUS_TEXT,
-} from "https://deno.land/x/oak_commons@0.3.0/status.ts";
+} from "https://deno.land/x/oak_commons@0.3.1/status.ts";
 export {
   type Data as SigningData,
   type Key as SigningKey,
   type KeyRing,
-} from "https://deno.land/x/oak_commons@0.3.0/types.d.ts";
+} from "https://deno.land/x/oak_commons@0.3.1/types.d.ts";
 export { contentType } from "https://deno.land/x/media_types@v3.0.2/mod.ts";
