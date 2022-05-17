8376a89d3a14d6e182c853af6eebf1b044bddd5e
Kitson Kelly <me@kitsonkelly.com>
2022-05-17 11:50:50 +1000

    feat: add searchParams to context

--- context.ts
+++ context.ts
@@ -1,5 +1,12 @@
 // Copyright 2022 the oak authors. All rights reserved.
 
+/**
+ * Contains the class {@linkcode Context} which provides context for the request
+ * and response to the request handler.
+ *
+ * @module
+ */
+
 import { type Cookies } from "./deps.ts";
 import { type Deserializer } from "./types.d.ts";
 
@@ -10,6 +17,8 @@ interface ContextOptions<BodyType, Params extends Record<string, string>> {
   request: Request;
 }
 
+/** An object that provides context for the associated request and response.
+ * This is passed as the first argument to every route handler. */
 export class Context<
   BodyType = unknown,
   Params extends Record<string, string> = Record<string, string>,
@@ -20,17 +29,31 @@ export class Context<
   #deserializer?: Deserializer<BodyType, Params>;
   #params: Params;
   #request: Request;
+  #requestUrl?: URL;
 
+  /** The instance of {@linkcode Cookies} that allows reading and setting of
+   * cookies on the request and response. */
   get cookies(): Cookies {
     return this.#cookies;
   }
 
+  /** Any {@linkcode Params} that have been parsed out of the URL requested
+   * based on the URL pattern string provided to the `Route`. */
   get params(): Params {
     return this.#params;
   }
 
+  /** The original {@linkcode Request} associated with this request. */
   get request(): Request {
     return this.#request;
+  }
+
+  /** Any {@linkcode URLSearchParams} associated with the request. */
+  get searchParams(): URLSearchParams {
+    if (!this.#requestUrl) {
+      this.#requestUrl = new URL(this.#request.url);
+    }
+    return this.#requestUrl.searchParams;
   }
 
   constructor(
