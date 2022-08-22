ea6aba7721bda921c08b30f016db1df4bcf8e9ab
Kitson Kelly <me@kitsonkelly.com>
2022-08-22 15:51:37 +1000

    chore: migrate deps from oak_commons to std

--- deps.ts
+++ deps.ts
@@ -1,14 +1,11 @@
 // Copyright 2022 the oak authors. All rights reserved.
 
-export { contentType } from "https://deno.land/std@0.142.0/media_types/mod.ts";
-export { Cookies } from "https://deno.land/x/oak_commons@0.3.1/cookies.ts";
 export {
   createHttpError,
   errors,
   type HttpError,
   isHttpError,
-} from "https://deno.land/x/oak_commons@0.3.1/http_errors.ts";
-export { accepts } from "https://deno.land/x/oak_commons@0.3.1/negotiation.ts";
+} from "https://deno.land/std@0.152.0/http/http_errors.ts";
 export {
   isClientErrorStatus,
   isErrorStatus,
@@ -18,7 +15,11 @@ export {
   isSuccessfulStatus,
   Status,
   STATUS_TEXT,
-} from "https://deno.land/x/oak_commons@0.3.1/status.ts";
+} from "https://deno.land/std@0.152.0/http/http_status.ts";
+export { contentType } from "https://deno.land/std@0.152.0/media_types/mod.ts";
+export { accepts } from "https://deno.land/std@0.152.0/http/negotiation.ts";
+
+export { Cookies } from "https://deno.land/x/oak_commons@0.3.1/cookies.ts";
 export {
   type Data as SigningData,
   type Key as SigningKey,
