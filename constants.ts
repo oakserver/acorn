ef8aff33d6fc0b1f6c59fbe05ddbc3627370db73
Kitson Kelly <me@kitsonkelly.com>
2024-07-27 20:15:28 +1000

    refactor: remove unused constants

--- constants.ts
+++ constants.ts
@@ -1,9 +1,4 @@
 // Copyright 2018-2024 the oak authors. All rights reserved.
 
-import { contentType } from "@std/media-types/content-type";
-
 export const BODYLESS_METHODS = ["GET", "HEAD"];
-export const CONTENT_TYPE_HTML = contentType("html")!;
-export const CONTENT_TYPE_JSON = contentType("json")!;
-export const CONTENT_TYPE_TEXT = contentType("text/plain")!;
 export const NOT_ALLOWED = Symbol.for("acorn.NotAllowed");
